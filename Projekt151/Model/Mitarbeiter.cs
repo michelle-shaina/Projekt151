51dfd83013d6e34f32e399ac95a07c21959ddba2
michelle-shaina <michelle.schaer21@gmail.com>
2021-04-07 17:18:46 +0200

    proper Login implemented and Logout

--- Projekt151/Model/Mitarbeiter.cs
+++ Projekt151/Model/Mitarbeiter.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,9 +9,13 @@ namespace Projekt151.Model
     public class Mitarbeiter
     {
         public string MitarbeiterId { get; set; }
+        [Required]
+        [MinLength(3, ErrorMessage = "Name has to be at least 2 Characters long")]
+        [StringLength(50, ErrorMessage = "Name is too long.")]
         public string MitarbeiterName { get; set; }
         public int StadtId { get; set; }
         public int RoleId { get; set; }
+        [MinLength(4, ErrorMessage = "Password has to be at least 4 Characters long")]
         public string Password { get; set; }
     }
 }
--- Projekt151/Model/Projekt.cs
+++ Projekt151/Model/Projekt.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Security.Policy;
 using System.Threading.Tasks;
@@ -9,6 +10,8 @@ namespace Projekt151.Model
     public class Projekt
     {
         public int ProjektId { get; set; }
+        [Required]
+        [MinLength(3, ErrorMessage = "Project Name hast to bee at least 3 Characters long")]
         public string ProjektName { get; set; }
         public string Beschreibung { get; set; }
         public string StartDate { get; set; }
