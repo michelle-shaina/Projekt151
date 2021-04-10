5b14b516b5c7eca776bee05f61c90fac3b647dde
michelle-shaina <michelle.schaer21@gmail.com>
2021-04-10 21:53:44 +0200

    added test

---
+++ Projekt151.Test/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace Projekt151.Test
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            using var cts = new TestContext()
+        }
+    }
+}
---
+++ Projekt151/Tests/Projekt151Tests.cs
@@ -0,0 +1,43 @@
+﻿using AngleSharp.Dom;
+using Bunit;
+using Bunit.Extensions;
+using Microsoft.Data.SqlClient.DataClassification;
+using Projekt151.Pages;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices.ComTypes;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Projekt151.Tests
+{
+    public class Projekt151Tests
+    {
+        [Fact]
+        public void ShouldComeHintWhenLoggedInWrong()
+        {
+            using var ctx = new TestContext();
+            var component = ctx.RenderComponent<Login>();
+
+            var btn = component.Find("button");
+            btn.Click();
+            var label = component.Find("label");
+            label.LocateNamespaceFor("Name").IsNullOrEmpty();
+            label.LocateNamespaceFor("Password").IsNullOrEmpty();
+
+            component.MarkupMatches(@"<p>There is no combination of the user and password found, please check the spelling an try again!<p>");
+        }
+        [Fact]
+        public void AdminLogin()
+        {
+            using var ctx = new TestContext();
+            var component = ctx.RenderComponent<Login>();
+
+            var btn = component.Find("button");
+            component.Find("input[aria-label=Name]").Change("Michelle");
+            component.Find("input[aria-label=Password]").Change("1234");
+            component.Find("form").Submit();
+        }
+    }
+}
